5666d7c05f231f2fd725fd750c6cb79414ac0359
RayChen0 <43762369+RayChen0@users.noreply.github.com>
2019-02-27 10:13:45 -0500

    Update lab2.c

--- lab2.c
+++ lab2.c
@@ -50,8 +50,6 @@ pthread_t network_thread;
 void *network_thread_f(void *);
 void Custum_Initial();
 void InitiateRow(int, int);
-char *MoveString(char *, int);
-void ActScroll(int, int, int, char *);
 int JudgeClass(char);
 char memory;
 
@@ -66,6 +64,7 @@ int main()
   struct sockaddr_in serv_addr;
   int count=0;
   int i, flag;
+  int currentIndex;
   struct usb_keyboard_packet packet;
   int transferred;
   char keystate[12];
@@ -73,6 +72,7 @@ int main()
   char writeStringBuffer1[MAX_PER_ROW+1];
   char writeStringBuffer2[MAX_PER_ROW+1];
   char selfBuffer[10][MAX_PER_ROW+1];
+
 
   if ((err = fbopen()) != 0) {
     fprintf(stderr, "Error: Could not open framebuffer: %d\n", err);
@@ -141,7 +141,7 @@ int main()
          }
 	 else if (packet.keycode[1] != 00){
            memory = packet.keycode[1];
-	   dispCharacter = keyValue(packet.modifiers, packet.keycode[1]);
+	        dispCharacter = keyValue(packet.modifiers, packet.keycode[1]);
 	 }
          else{
            continue;
@@ -149,15 +149,20 @@ int main()
          
          /* Assume we have a function JudgeClass to judge whether it's a control or a letter, return flag=0 if it is a letter, flag!=0 if it is a function  */
          flag = JudgeClass(packet.keycode[0]);
-         if (flag==1){ /* if Enter is pressed */
-           
-           
+         
+         
+         if (flag == 1){ /* if Enter is pressed */  
            /* Assume the string to write is less than BUFFER_SIZE */
            write(sockfd, writeString, strlen(writeString)); /* send to server*/
            
            /* whenever put in long string */
            /* check the length first */
-            if (strlen(writeString)<=MAX_PER_ROW) {/* if length is shorter than a single line width */
+            if (!strlen(writeString)) {
+                continue;
+            }
+            
+            else if (strlen(writeString)<=MAX_PER_ROW) {/* if length is shorter than a single line width */
+                write(sockfd, writeString, strlen(writeString)); /* send to server*/
                 strcpy(selfBuffer[row2-HIG_BOUND_SEC],writeString);
                 writeString[0] = '\0';
                 fbputs(selfBuffer[row2-HIG_BOUND_SEC],row2,0);
@@ -175,10 +180,10 @@ int main()
                   selfBuffer[9][0]='\0';
                 }
                 /* finish scroll */
-
-                
             }
+            
             else{/* if length is larger than one line, split them into two lines */
+                write(sockfd, writeString, strlen(writeString)); /* send to server*/
                 strncpy(selfBuffer[row2-HIG_BOUND_SEC],writeString,MAX_PER_ROW);
                 selfBuffer[row2-HIG_BOUND_SEC][MAX_PER_ROW]='\0';
                 fbputs(selfBuffer[row2-HIG_BOUND_SEC],row2,0);
@@ -217,39 +222,138 @@ int main()
                 writeString[0] = '\0';
             }
             InitiateRow(HIG_BOUND_THI,LOW_BOUND_THI);
-            count = 0;
-            currentCol = 0;
+            count = 0;        /* counting the length of input string */
+            currentCol = 0;   /* counting the positon of cursor */
             currentRow = HIG_BOUND_THI;
+            fbputcursor(0x20,currentRow, currentCol);
          }
+
+
          else if (flag == 0){/* if a character is pressed */
-           /* we only allow maximum 128 character*/
-           if (count<128) {
-              fbputchar(dispCharacter, currentRow, currentCol);
-	            writeString[count] = dispCharacter;
-              writeString[count+1]='\0';
-	            currentCol++;
-	            count++;
-	            printf("count2 = %d, col2= %d, row2= %d\n", count, currentCol, currentRow);
-              if (currentCol==MAX_PER_ROW && currentRow==HIG_BOUND_THI) {
-                currentCol = 0;
-                currentRow = LOW_BOUND_THI;
-              }
-           }
-           else
-           {
-             continue;
-           }
-                      
+           /* we only allow maximum 127 character, remain one position for cursor*/
+            if (count<BUFFER_SIZE-1) {
+	              /* Add the char to the middle of a string */
+                for( i = strlen(writeString); i > currentIndex; i--){
+                  writeString[i]=writeString[i-1];
+                }
+                writeString[i]=dispCharacter;
+                count++;
+                writeString[count]='\0';
+                /* end of adding char to a string */
+                if (currentCol==MAX_PER_ROW-1 && currentRow == HIG_BOUND_THI) {
+                  currentCol = 0;
+                  currentRow = LOW_BOUND_THI;
+                }
+                else
+                {
+                  currentCol++;
+                }
+                
+                currentIndex = MAX_PER_ROW*(currentRow-HIG_BOUND_THI)+currentCol;
+	              printf("count2 = %d, col2= %d, row2= %d\n", count, currentCol, currentRow);
+            }          
 	       }
-         else if (flag == 2) {/* if delete is pressed */
-           /*Do other function here*/
+         else if (flag == 2) {/* if backspace is pressed */
+           /*We delete the char at current index -1 and push chars behind it forward*/
+            if (!(currentRow == HIG_BOUND_THI && currentCol == 0)) {
+                /* remove some character from a string */
+                for( i = currentIndex-1; i < strlen(writeString); i++){
+                  writeString[i]=writeString[i+1];
+                }
+                /* finish remove a charactor from string */
+
+                count--;
+                if (currentCol == 0 && currentRow == LOW_BOUND_THI) {
+                    currentRow = HIG_BOUND_THI;
+                    currentCol = MAX_PER_ROW-1;
+                }
+                else
+                {
+                    currentCol--;
+                }
+                currentIndex = MAX_PER_ROW*(currentRow-HIG_BOUND_THI)+currentCol;
+                currentCharacter = writeString[currentIndex];
+            }
+            
          }
-         else if (flag == 3){/* if direction key is pressed */
-	 }
-         else if (packet.keycode[0] == 0x29) { /* ESC pressed? */
-	    break;
+         else if (flag == 3){/* if left direction key is pressed */
+            /* Only work when it's not on the most left of first row */
+            if (!(currentRow == HIG_BOUND_THI && currentCol == 0)) {
+                if (currentCol == 0 && currentRow == LOW_BOUND_THI) {
+                    currentRow = HIG_BOUND_THI;
+                    currentCol = MAX_PER_ROW-1;
+                }
+                else
+                {
+                  currentCol--;
+                }
+                  currentIndex = MAX_PER_ROW*(currentRow-HIG_BOUND_THI)+currentCol;
+                  currentCharacter = writeString[currentIndex];
+            }
+          }
+          
+          else if (flag == 4) {/* if right direction key is pressed */
+            /* Only work when it's not on the most right of second row */
+            /* Only work when it's within the range of text */
+            if (!(currentRow == LOW_BOUND_THI && currentCol == MAX_PER_ROW-1) && (currentIndex<=count)) {
+                if (currentRow == HIG_BOUND_THI && currentCol == MAX_PER_ROW-1 ) {
+                    currentRow = LOW_BOUND_THI;
+                    currentCol = 0;
+                }
+                else
+                {
+                    currentCol++;
+                }
+                currentIndex = MAX_PER_ROW*(currentRow-HIG_BOUND_THI)+currentCol;
+            }
+          }
+
+          else if (flag == 5) {/* if delete is pressed */
+           /*We delete the char at current index and push chars behind it forward*/
+            if (!(currentRow == HIG_BOUND_THI && currentCol == 0)) {
+                /* remove some character from a string */
+                for( i = currentIndex; i < strlen(writeString); i++){
+                  writeString[i]=writeString[i+1];
+                }
+                /* finish remove a charactor from string */
+
+                count--;
+                if (currentCol == 0 && currentRow == LOW_BOUND_THI) {
+                    currentRow = HIG_BOUND_THI;
+                    currentCol = MAX_PER_ROW-1;
+                }
+                else
+                {
+                    currentCol--;
+                }
+                currentIndex = MAX_PER_ROW*(currentRow-HIG_BOUND_THI)+currentCol;
+                currentCharacter = writeString[currentIndex];
+            }
+          
+          /* Now we show the whole message */
+          
+          if (flag ==2 || flag ==5) {
+                if (strlen(writeString)<=MAX_PER_ROW) {
+                  fbputs(writeString, currentRow, 0);
+                }
+                else
+                {
+                  strncpy(writeStringBuffer1,writeString,MAX_PER_ROW);
+                  writeStringBuffer1[MAX_PER_ROW]='\0';
+                  fbputs(writeStringBuffer,HIG_BOUND_THI,0);
+                  
+                  strncpy(writeStringBuffer2,writeString,strlen(writeString)-MAX_PER_ROW);
+                  writeStringBuffer2[strlen(writeString)-MAX_PER_ROW]='\0';
+                  fbputs(writeStringBuffer2,LOW_BOUND_THI,0);                          
+                  }
+          }
+          if (flag == 2 || flag ==3 || flag ==4 || flag == 5) {
+                  fbputcursor(writeString[currentIndex], currentRow, currentCol);
+          }
          }
-	 fbputcursor(0x20, currentRow, currentCol);
+          
+  else if (packet.keycode[0] == 0x29) { /* ESC pressed? */
+	      break;}
     }
   }
 }
@@ -281,40 +385,32 @@ void InitiateRow(int min, int max){
   }	
 }
 
-
-
-/* Scroll the screen if condition matches*/
-void ActScroll(int minRow, int maxRow, int colPerRow, char *myString){
-  /* myString should already been modified as exactly like we want to show in the screen*/
-   int lineNum;
-   int count=0;
-   int row, col;
-   InitiateRow(minRow, maxRow);
-   lineNum = maxRow - minRow;
-   for (row=minRow;row<maxRow;row++){
-      for (col=0;col<colPerRow;col++){
-        fbputchar(myString[count],row,col);
-        count++;
-      }
-   }   
-}
-
-/* Move all element in a string forward n, others become '\0' */
-char *MoveString(char toBeMoveString[], int moveLength){
-  char *movedString;
-  movedString=&toBeMoveString[moveLength];
-  return movedString;
-}
-
 int JudgeClass(char dispCharacter){
   switch (dispCharacter){
+    /*enter */
     case 0x28:
       return 1;
       break;
     case 0x58:
       return 1;
       break;
-      default:
+    /*back */
+    case 0x2a:
+      return 2;
+      break;
+    /* left*/
+    case 0x50:
+      return 3;
+      break;
+    /* right */
+    case 0x4F:
+      return 4;
+      break;
+    /* delete */
+    case 0x4c:
+      return 5;
+      break;
+    default:
       return 0;
   }
 }
@@ -323,45 +419,68 @@ int JudgeClass(char dispCharacter){
 void *network_thread_f(void *ignored)
 {
   char recvBuf[BUFFER_SIZE];
-  char tempBuf1[MAX_PER_ROW+1];
-  char tempBuf2[MAX_PER_ROW+1];
-  char myBuff[700];
+  char readBuffer[10][65];
   int n;
-  int countLength=0;
   int i;
-  int tempRow=1;
+  int readRow=HIG_BOUND_FIR;
   /* Read: Receive data */
   /* Assume the length is smaller than BUFFER_SIZE*/
   while ( (n = read(sockfd, &recvBuf, BUFFER_SIZE - 1)) > 0 ) {
     recvBuf[n] = '\0';
     printf("%s", recvBuf);
-    for (i=0;i<strlen(recvBuf);i++){
-      myBuff[i+countLength]=recvBuf[i];
-    }
-    if (n < MAX_PER_ROW || n==MAX_PER_ROW){  
-      fbputs(recvBuf, tempRow, 0);
-      tempRow++;
-    }
-    else{
-      for (i=0;i<MAX_PER_ROW;i++){
-        tempBuf1[i]=recvBuf[i];
+
+    if (n<=MAX_PER_ROW) {/* if length is shorter than a single line width */
+      strcpy(readBuffer[readRow-HIG_BOUND_FIR],recvBuf);
+      fbputs(readBuffer[readRow-HIG_BOUND_FIR],readRow,0);
+      readRow++;
+
+                /* if it reaches to the boundary, then scroll */
+      if (readRow > LOW_BOUND_FIR) {
+          InitiateRow(HIG_BOUND_FIR,LOW_BOUND_FIR);
+          readRow = LOW_BOUND_FIR;
+          for(i = 0; i < 9 ; i++){
+              strcpy(readBuffer[i],readBuffer[i+1]);
+              fbputs(readBuffer[i],HIG_BOUND_FIR + i,0);
+          }
+          readBuffer[9][0]='\0';
       }
-	tempBuf1[i]='\0';
-      for (i=0;i < n-MAX_PER_ROW;i++){
-        tempBuf2[i]=recvBuf[i];
-      }
-	tempBuf2[i]='\0';
-      fbputs(tempBuf1, tempRow, 0);
-      tempRow++;
-      fbputs(tempBuf2, tempRow, 0);
-      tempRow++;
-    }	  
-    /* Scroll up when it is full */
-    if (tempRow>LOW_BOUND_FIR){
-      strcpy(myBuff, MoveString(myBuff,MAX_PER_ROW));
-      ActScroll(HIG_BOUND_FIR,LOW_BOUND_FIR,MAX_PER_ROW, myBuff);
+                /* finish scroll */
     }
-  }
+      else{/* if length is larger than one line, split them into two lines */
+          strncpy(readBuffer[readRow-HIG_BOUND_FIR],recvBuf,MAX_PER_ROW);
+          readBuffer[readRow-HIG_BOUND_FIR][MAX_PER_ROW]='\0';
+          fbputs(readBuffer[row2-HIG_BOUND_FIR],readRow,0);
+          readRow++;
+          /* if it reaches to the boundary, then scroll */
+          if (readRow > LOW_BOUND_FIR) {
+            InitiateRow(HIG_BOUND_FIR,LOW_BOUND_FIR);
+            readRow = LOW_BOUND_FIR;
+            for(i = 0; i < 9 ; i++){
+              strcpy(readBuffer[i],readBuffer[i+1]);
+              fbputs(readBuffer[i],HIG_BOUND_FIR + i,0);
+            }
+          readBuffer[9][0]='\0';
+          }
+          /* finish scroll */
+
+          strncpy(readBuffer[readRow-HIG_BOUND_FIR],recvBuf+MAX_PER_ROW,strlen(recvBuf)-MAX_PER_ROW);
+          readBuffer[readRow-HIG_BOUND_FIR][strlen(recvBuf)-MAX_PER_ROW]='\0';
+          fbputs(readBuffer[row2-HIG_BOUND_FIR],readRow,0);
+          readRow++;
+
+          /* if it reaches to the boundary, then scroll */
+          if (readRow > LOW_BOUND_FIR) {
+            InitiateRow(HIG_BOUND_FIR,LOW_BOUND_FIR);
+            readRow = LOW_BOUND_FIR;
+            for(i = 0; i < 9 ; i++){
+              strcpy(readBuffer[i],readBuffer[i+1]);
+              fbputs(readBuffer[i],HIG_BOUND_FIR + i,0);
+            }
+          readBuffer[9][0]='\0';
+          }
+          /* finish scroll */
+       }
+    }
   return NULL;
 }
 
